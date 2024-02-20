3927fa6d6cf92547eee745c7427d95c049cef3d5
AdamAung <104379078+AdamAung@users.noreply.github.com>
2024-02-20 13:25:45 +0630

    remove: swiper js for performance issue

--- src/Home/SwiperMovies.tsx
+++ src/Home/SwiperMovies.tsx
@@ -1,9 +1,3 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, EffectCoverflow } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { MovieResult, fetchUpComingMoive } from "@/Utils/FetchAPI";
@@ -11,23 +5,6 @@ import getImagePath from "@/Utils/GetImagePath";
 
 const SwiperMovies = () => {
   const [upcoming, setUpComing] = useState([]);
-  //   const swiper = new Swiper(".swiper", {
-  //     modules: [Navigation, Pagination],
-
-  //     direction: "vertical",
-  //     loop: true,
-
-  //     // If we need pagination
-  //     pagination: {
-  //       el: ".swiper-pagination",
-  //     },
-
-  //     // Navigation arrows
-  //     navigation: {
-  //       nextEl: ".swiper-button-next",
-  //       prevEl: ".swiper-button-prev",
-  //     },
-  //   });
 
   useEffect(() => {
     const upComingMovies = async () => {
@@ -38,48 +15,24 @@ const SwiperMovies = () => {
   }, []);
 
   return (
-    <div className="flex justify-center">
-      <Swiper
-        className="swiper_container"
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={2}
-        pagination={{ el: ".swiper-pagination", clickable: true }}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 2.5,
-        }}
-      >
-        {upcoming.map((data: MovieResult) => (
-          <SwiperSlide>
-            <div className="flex">
+    <div className="flex justify-center ">
+      <div className="flex w-[80%] snap-mandatory snap-start overflow-hidden overflow-scroll">
+        <div className="flex ">
+          {upcoming.map((data: MovieResult) => (
+            <div className="w-[20rem] h-fit relative">
               <img
                 src={getImagePath(data.poster_path)}
-                className="rounded-sm"
-                width={300}
+                className="rounded-md z-30 hover:z-10 hover:brightness-50"
+                width={320}
                 height={500}
               />
-              <span className="text-center w-[400px] pt-2 text-xl font-medium font-roboto text-white">
+              <span className=" absolute w-[400px] top-[50%] left-2 z-20 pt-2 text-xl font-medium font-roboto text-white">
                 {data.original_title}
               </span>
             </div>
-          </SwiperSlide>
-        ))}
-        <div className="slider-controller">
-          <div className="swiper-button-prev"></div>
-          <div className="swiper-button-next"></div>
-          <div className="swiper-pagination"></div>
+          ))}
         </div>
-      </Swiper>
+      </div>
     </div>
   );
 };
