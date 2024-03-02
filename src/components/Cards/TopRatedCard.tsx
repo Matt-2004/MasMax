e2162f674515a36ffc68bca35bd6b677f725f5a5
AdamAung <104379078+AdamAung@users.noreply.github.com>
2024-03-02 22:06:34 +0630

    add responsive for mobile

--- src/components/Cards/TopRatedCard.tsx
+++ src/components/Cards/TopRatedCard.tsx
@@ -21,25 +21,25 @@ const MovieCard = () => {
   }, []);
 
   return (
-    <div className='w-[100%] mt-16 flex justify-center'>
-      <div className='flex flex-col'>
-        <div className='flex justify-between'>
-          <div className='mb-8  font-bold  md:text-2xl sm:text-xl text-[#2eade7]'>
-            Top Rated
+    <div className="w-[100%] mt-16 max-sm:mt-1 flex justify-center">
+      <div className="flex flex-col">
+        <div className="flex justify-between mx-2">
+          <div className="mb-8  font-bold  md:text-2xl sm:text-xl max-sm:text-xl text-[#2eade7]">
+            Popular
           </div>
           <div
-            className='cursor-pointer text-md underline text-white'
+            className="cursor-pointer text-md underline text-white"
             onClick={() => naviAndSetItem()}
           >
             see more
-            <span>
+            <span className="max-sm:text-md">
               <div /> // arrowRightAlt
             </span>
           </div>
         </div>
-        <div className='flex cursor-pointer lg:w-[62.5rem] md:w-[48.12rem] sm:w-[40rem] justify-between'>
+        <div className="flex flex-wrap gap-4 cursor-pointer  lg:w-[62.5rem] md:w-[48.12rem] sm:w-[40rem] justify-between max-sm:justify-evenly">
           {rated.slice(0, 4).map((p: MovieResult) => (
-            <div key={p.id} className='relative'>
+            <div key={p.id} className="relative">
               <div
                 onClick={() => {
                   localStorage.setItem("id", p.id.toString()),
@@ -49,8 +49,8 @@ const MovieCard = () => {
                 <img
                   src={getImagePath(300, p.poster_path)}
                   alt={p.title}
-                  loading='lazy'
-                  className='rounded-md hover:scale-110 transition-all duration-300 lg:w-[14.37rem] md:w-[11.25rem] sm:w-[8.75rem]'
+                  loading="lazy"
+                  className="rounded-sm hover:scale-110 transition-all duration-300 lg:w-[14.37rem] md:w-[11.25rem] sm:w-[8.75rem] max-sm:w-[9.3rem]"
                 />
               </div>
             </div>
--- src/components/Detils/MovieDetils.tsx
+++ src/components/Detils/MovieDetils.tsx
@@ -23,34 +23,26 @@ const MovieDetils = () => {
     const valueFromGetItem: any = localStorage.getItem("id");
     async function fetchDetils() {
       const response = await fetchMovieDetils(valueFromGetItem);
-      if (response) {
+      const res = await fetchVideo(valueFromGetItem);
+      if (response && res) {
         setDetils(response);
+        setVideos(res);
       } else {
         fetchDetils();
       }
     }
-
-    async function fetchingVideo() {
-      const response = await fetchVideo(valueFromGetItem);
-      if (response) {
-        setVideos(response);
-      } else {
-        fetchingVideo();
-      }
-    }
     fetchDetils();
-    fetchingVideo();
   }, []);
 
   return (
     <>
       {detils ? (
-        <div className='relative h-[100%] w-[100%] bg-[#26262e]'>
+        <div className="relative h-[100%] w-[100%] bg-[#26262e]">
           <BackDropPath backdrop_path={detils.backdrop_path} />
-          <div className='md:absolute  top-[9%] left-[15%] sm:left-[10%] flex'>
-            <section className='flex lg:w-[56.25rem] md:w-[37.5rem] sm:w-[31.25rem] max-sm:w-[25rem] justify-evenly '>
+          <div className="md:absolute  top-[9%] left-[15%] sm:left-[10%] flex">
+            <section className="flex lg:w-[56.25rem] md:w-[37.5rem] sm:w-[31.25rem] max-sm:w-[25rem] justify-evenly ">
               <Poster_path poster_path={detils.poster_path} />
-              <div className='max-sm:w-[23rem] md:w-[100%] sm:w-[27rem] sm:flex sm:justify-center sm:mt-3 max-sm:mt-4 max-sm:flex max-sm:justify-center'>
+              <div className="max-sm:w-[20rem] md:w-[100%] sm:w-[27rem] sm:flex sm:justify-center sm:mt-3 max-sm:mt-4 max-sm:flex max-sm:justify-center">
                 <RightInfoContainer>
                   <Title
                     title={detils.title}
@@ -61,9 +53,7 @@ const MovieDetils = () => {
                   <OverView overview={detils.overview} />
 
                   <LanguageAndVoteContainer>
-                    <OriginalLanguage
-                      original_language={detils.original_language}
-                    />
+                    <OriginalLanguage />
                     <VoteAverage vote_average={detils.vote_average} />
                   </LanguageAndVoteContainer>
                 </RightInfoContainer>
