b63beb5c183c2008d4a2e579febfe00896c2da96
Mas <wai450013956@gmail.com>
2024-03-04 14:22:15 +0630

    remove seemorecoms that deleted

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Suspense, lazy } from "react";
 const SignUp = lazy(() => import("./components/Register&login/SignUp"));
 const SignIn = lazy(() => import("./components/Register&login/SignIn"));
 
-const SeeMoreComs = lazy(() => import("./components/SeeMore/SeeMoreComs"));
 const MovieDetils = lazy(() => import("./components/Detils/MovieDetils"));
 const MoviePage = lazy(() => import("./components/Search/SearchPage"));
 const GenrePage = lazy(() => import("./components/Genres/GenrePage"));
@@ -13,7 +12,6 @@ const GenrePage = lazy(() => import("./components/Genres/GenrePage"));
 const elements = [
   { path: "/search/:searchId", element: <MoviePage /> },
   { path: "movie/:movieId", element: <MovieDetils /> },
-  { path: "/seemore/page/:id", element: <SeeMoreComs /> },
   { path: "/genres/:genreId", element: <GenrePage /> },
   { path: "/register", element: <SignUp /> },
   { path: "/login", element: <SignIn /> },
