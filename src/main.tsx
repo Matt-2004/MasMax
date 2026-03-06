b0da9c6fc7e0ccd0847fb43a17a0e47f5e0fca41
Cap <wai450013956@gmail.com>
2026-03-06 21:13:31 +0700

    Add @vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/next";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    <Analytics />
+  </React.StrictMode>,
 );
